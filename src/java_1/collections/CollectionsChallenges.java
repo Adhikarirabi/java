252613e27e1cdd505b3f9c5cd16889611c20ead8
Adhikarirabi <66221135+Adhikarirabi@users.noreply.github.com>
2021-03-07 21:11:17 -0600

    initial commit

--- src/java_1/collections/CollectionsChallenges.java
+++ src/java_1/collections/CollectionsChallenges.java
@@ -8,33 +8,56 @@ public class CollectionsChallenges {
 //		Complete the code challenges for the following static methods
 	}
 
-	int findVal(int val, ArrayList<Integer> arrList) {
+	static int findVal(int val, ArrayList<Integer> arrList) {
 //		find a value in the input array list.  return it if found.  If not, return 0.
-		return 0;
+		if(arrList.contains(val)){
+			return val;
+
+		}
+		else return 0;
 	}
 
 	static List convertArrToList(Object[] array) {
 //		convert the array to a list and return it.
-		return null;
+		return Arrays.asList(array);
+
 	}
 
 	static Set<String> listToSet(List<String> states) {
 //		convert a list of US states to a set.  return the set.
-		return null;
+		Set<String> setState = new HashSet<>(states);
+		return setState;
 	}
 
 	static int addToList(int x, ArrayList<Integer> numList) {
-		// add the x input to the array list.  return the value if succcessfully added.
-		return x;
+		// add the x input to the array list.  return the value if successfully added.
+		numList.add(x);
+		if (numList.contains(x)){
+			return x;
+
+		}
+		else {
+			return 0;
+		}
+
 	}
 
 	static void removeFromList(int x, ArrayList<Integer> numList) {
 		// remove the input parameter value x from the numList.
+		if (numList.contains(x)){
+			numList.remove(numList.indexOf(x));
+			System.out.println(numList);
+		}
+
 	}
 
 	static int insertIntoList(int x, int i, ArrayList<Integer> numList) {
 		// insert the the value x at index position i for the input numList.  If the index position is outside the bounds of the current numList, add it to the end of the array list.
-		return x;
+		if (numList.size()< i){
+			numList.add(x);
+		}
+		numList.add(i, x);
+		return (numList.get(i));
 	}
 
 	static List<Integer> reverseNums(ArrayList<Integer> nums) {
@@ -42,7 +65,8 @@ public class CollectionsChallenges {
 		reverse the values of the nums list
 		return the reversed array list.
 		*/
-		return null;
+		Collections.reverse(nums);
+		return nums;
 	}
 
 	static LinkedList<String> reverseLinkedList(LinkedList<String> linkedList, boolean mutate) {
@@ -50,11 +74,23 @@ public class CollectionsChallenges {
 /*		reverse the linked list.  If the mutate boolean is set to true, reverse a copy of the linked list without mutating the original list.  If mutate is false, reverse the original linkedList in place.
 		example : ["red","orange","yellow","green","blue","indigo","violet"] => reverseLinkedList => [violet, indigo, blue, green, yellow, orange, red]
  */
-		return linkedList;
+		if (mutate){
+			LinkedList<String>copiedList = new LinkedList<String>();
+			copiedList = (LinkedList)linkedList.clone();
+			Collections.reverse(copiedList);
+			return copiedList ;
+		}
+		else{
+			Collections.reverse(linkedList);
+			return linkedList;
+		}
+
 	}
 
 	static ArrayList<String> removeDuplicates(ArrayList<String> names) {
 //		remove duplicates from the list of name strings.  return a new array list with the unique elements.
+
+
 		return names;
 	}
 
