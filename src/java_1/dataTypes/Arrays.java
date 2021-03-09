8b1b831e13907e80c8b8d0a2e4d9b70e6a834ff7
Adhikarirabi <66221135+Adhikarirabi@users.noreply.github.com>
2021-03-08 22:55:43 -0600

    completed

--- src/java_1/dataTypes/Arrays.java
+++ src/java_1/dataTypes/Arrays.java
@@ -1,13 +1,20 @@
 package java_1.dataTypes;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 class Arrays {
     int[] nums = new int[]{10,23,3,4,5,2,1};
     String[] greetings = new String[]{"Hello","Howdy","Hallo","Hola","Bonjour","Ciao","أهلا و سهلا"};
     int[] bits = new int[]{0,1,1,1,0,0,0,1,1,1,1,1,0,1,0,0,1,0};
     String[] colors = new String[]{"bluebird","yellow bumblebee","black spider", "green monkey","pink","great gatsby","feeding frenzy","black bird","Matterhorn","Great White Lakes","torah","white bible","green quran", "red herring", "Black Sea"};
 
-    void printNums() {
+    public void printNums() {
 //        Programmatically print the nums array to the console.  Print each number on a new line.  The nums array is defined on this class.
+        for(int i : nums){
+            System.out.println(i + "\n");
+        }
     };
 
     void greet(String name) {
@@ -17,18 +24,43 @@ class Arrays {
                "ABRAM" => greet() => "Hola Abram"
                "maTT => greet() => "أهلا و سهلا Matt"
  */
+
+            for(int i = 0; i < greetings.length; i++){
+                int random = (int)(greetings.length * Math.random());
+                System.out.println(greetings[random] + " " + name);
+        }
     };
 
     boolean[] bitsToBinaries() {
 //        Programmatically convert the bit array of 1's and 0's into a binary array of true and false. return the 'converted' array.
 //        ex : bitsToBinaries() => [false,true,true,true,false,false,false,true,true,true,true,true,false,true,false,false,true,false]
-            return null;
+        boolean [] b2b = new boolean [bits.length];
+        for(int i = 0; i < bits.length; i++){
+            if(bits[i] == 0){
+                b2b [i] = false;
+            }
+            else {
+                b2b [i] = true;
+            }
+        }
+        return b2b;
     };
 
     String[] keepColors() {
 //        In this class is an array called "colors".  Programmatically remove the elements that do NOT have a color associated with it.  Return the filtered color array of elements that have a color associated with it.
 //        ex : keepColors() => ["bluebird","yellow bumblebee","black spider", "green monkey","pink","black bird","Great White Lakes","white bible","green quran", "red herring", "Black Sea"]
-        return null;
+        String[] colors = new String[]{"red", "blue", "yellow", "white", "purple"};
+        List<String> listOfColors = new ArrayList<>();
+       for (String complextion: listOfColors ){
+           for (String verify : colors){
+               if(complextion.toLowerCase().contains(verify)){
+                   listOfColors.add(complextion);
+               }
+           }
+        }
+        String [] ColorsOnly = new String [listOfColors.size()];
+       ColorsOnly = listOfColors.toArray((ColorsOnly));
+       return ColorsOnly;
     }
 
 }
--- src/java_1/dataTypes/DataTypes.java
+++ src/java_1/dataTypes/DataTypes.java
@@ -1,50 +1,82 @@
+
 package java_1.dataTypes;
+import java.util.Arrays;
 
 class DataTypes {
 
-//		boolean number = 2021;
-//		byte yearOfBirth = 1977;
-//		Array nums = [3,4,5,6,7,8,10,12];
-//		Object movieName = "Meet The Parents";
-//		double male = 'm';
-//		triple triplet = 3L;
-//		float female = 'f';
-//		short decimalVal = .0000000000001f;
-//		long halfByte = 0101;
+	int number = 2021;
+	int yearOfBirth = 1977;
+	int [] nums = {3,4,5,6,7,8,10,12};
+	String movieName = "Meet The Parents";
+	char male = 'm';
+	long triplet = 3L;
+	char female = 'f';
+	double decimalVal = .0000000000001f;
+	byte halfByte = 0101;
 
 
 	public static void main(String[] args) {
 		//the class fields defined in this DataTypes class have mistakes with either the data type, or the syntax.  Fix the data types so the main method correctly displays the value of all of the variables
-		//For the methods below in this DataTypes class, determine the appropriate datatypes for the inputs and the outputs
+
+		//For the methods below in this DataTypes class, determine the appropriate data types for the inputs and the outputs
+		yearsToMonths(7);
+		monthsToHours(12);
+		daysToSeconds(344);
+		centuriesToSeconds(3);
+		dollarsToBitcoin(50000);
+		canDrive(18);
+		DataTypes data = new DataTypes();
+		System.out.println(Arrays.toString(data.getDataTypes(4, true, 1000000)));
+
 	}
 
-	void yearsToMonths(Object years) {
+public static void yearsToMonths(int years) {
 		//	convert years to months using appropriate data types for the inputs and the outputs, 2 years => 24 months
+		int months = 12 * years;
+		System.out.println(months);
 	}
 
-	void monthsToHours(Object months) {
+	public static void monthsToHours(int months) {
 		//	convert months to hours using appropriate data types for the inputs and the outputs, 8 months => 17520 hours
+		long hours = months * 30 * 24;
+		System.out.println(hours);
 	}
 
-	void daysToSeconds(Object days) {
+	public static  void daysToSeconds(int days) {
 		//	convert days to seconds using appropriate data types for the inputs and the outputs, 2 days => 172800 seconds
+		int seconds = days * 86400;
+		System.out.println(seconds);
 	}
 
-	void centuriesToSeconds(Object centuries) {
+	public static void centuriesToSeconds(int centuries) {
 		//	convert centuries to seconds using appropriate data types for the inputs and the outputs, 1 century => 3.1556952 x 10^9 seconds
+	long secToCen = centuries * 3153600000L;
+		System.out.println(secToCen);
 	}
 
-	void dollarsToBitcoin(Object dollars) {
+	public static void dollarsToBitcoin(int dollars) {
 		//	convert (usd) dollars $ to bitcoin using appropriate data types for the inputs and the outputs, As of 1/2021, 1 bitcoin ~=> $50000.00
+		float dolToBit = dollars * 0.000022f;
+		System.out.println(dolToBit);
 	}
 
-	void canDrive(Object age) {
+	public static void canDrive(int age) {
 		//determine if the age passed in is ready to drive, return a value indicating whether or not the age passed in is ready to drive.
+		if(age >= 18) {
+			System.out.println("Person is eligible to drive");
+		} else {
+			System.out.println("Person is not eligible to drive");
+		}
 	}
 
-	String[] getDataTypes(int num1, boolean bool1, long long1) {
-//		Use Java to get the data type of the three input variables; and return a string representation of the data types of the three variables as an array.  Do NOT hard code the answers here.  Make sure to use Java to programmatically get the data types.
-		return new String[]{null,null,null};
+	  String[] getDataTypes(int num1, boolean bool1, long long1) {
+//	Use Java to get the data type of the three input variables; and return a string representation of the data types of the three variables as an array.  Do NOT hard code the answers here.  Make sure to use Java to programmatically get the data types.
+
+	String Num1 = ((Object)num1).getClass().getName();
+		String Bol1 = ((Object)bool1).getClass().getName();
+		String Long1 = ((Object)long1).getClass().getName();
+
+		return new String[]{Num1, Bol1, Long1};
 	}
 
 }
